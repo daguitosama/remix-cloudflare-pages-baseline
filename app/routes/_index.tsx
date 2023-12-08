ef62c237e8413c68f4d42de95ac00783036414a4
Dago <tocarralero@gmail.com>
2023-12-08 12:36:03 -0500

    baseline

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,39 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: "Dago's Remix CF.Pages baseline" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
 };
 
+type LoaderData = {
+    beer: string;
+};
+
+export async function loader({ request, context }: LoaderFunctionArgs) {
+    return json<LoaderData>({
+        beer: context.env.BEER,
+    });
+}
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    const { beer } = useLoaderData<typeof loader>();
+    return (
+        <div className='min-h-screen w-full pt-32'>
+            <div className='grid gap-8  justify-center mx-auto text-center'>
+                <div>
+                    <p className='text-sm font-medium'>Distilled since christmas 2023</p>
+                </div>
+                <div>
+                    <h1 className='text-4xl'>Dago's Remix C.Pages baseline template</h1>
+                </div>
+                <div>
+                    <p className='text-2xl'>My beer from environment: {beer}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- server.ts
+++ server.ts
@@ -3,11 +3,28 @@ import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
 
 if (process.env.NODE_ENV === "development") {
-  logDevReady(build);
+    logDevReady(build);
+}
+
+export interface Env {
+    BEER: string;
+}
+declare module "@remix-run/server-runtime" {
+    interface AppLoadContext {
+        //  your context secret sauce goes here
+        env: Env;
+    }
 }
 
 export const onRequest = createPagesFunctionHandler({
-  build,
-  getLoadContext: (context) => ({ env: context.env }),
-  mode: build.mode,
+    build,
+    getLoadContext: (context) => {
+        if (typeof context.env.BEER != "string") {
+            throw new Error("Environment Load Error: $BEER missing");
+        }
+        return {
+            env: context.env,
+        };
+    },
+    mode: build.mode,
 });
